73d41eb3d0b4c5a406b1d0d91fc5da6d115384e3
Saul Johnson <saul.a.johnson@gmail.com>
2018-02-18 22:11:05 +0000

    Add assembly info

--- Ampasamp/Properties/AssemblyInfo.cs
+++ Ampasamp/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ampasamp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A minimal password sampler.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Saul Johnson")]
 [assembly: AssemblyProduct("Ampasamp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Saul Johnson 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
