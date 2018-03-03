738ca491e861005d57754088ef93f2f5e096c501
Saul Johnson <saul.a.johnson@gmail.com>
2018-03-03 18:51:22 +0000

    Fix critical bug

--- Ampasamp/Program.cs
+++ Ampasamp/Program.cs
@@ -89,7 +89,7 @@ namespace Ampasamp
             result = result && (str.CountOthers() >= policy.Others);
             result = result && (str.CountClasses() >= policy.Classes);
             result = result && (policy.Repetitions == -1 || (str.CountRepetitions() <= policy.Repetitions));
-            result = result && (policy.Repetitions == -1 || (str.CountConsecutives() <= policy.Consecutives));
+            result = result && (policy.Consecutives == -1 || (str.CountConsecutives() <= policy.Consecutives));
 
             // Dictionary check.
             if (policy.Dictionary != null && policy.Dictionary != string.Empty)
