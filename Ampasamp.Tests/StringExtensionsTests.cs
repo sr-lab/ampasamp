0f573970a46d19d77a7f50f6b3f153d094632c58
Saul Johnson <saul.a.johnson@gmail.com>
2018-05-11 13:00:15 +0100

    Add more tests to Ampasamp

--- Ampasamp.Tests/StringExtensionsTests.cs
+++ Ampasamp.Tests/StringExtensionsTests.cs
@@ -7,6 +7,97 @@ namespace Ampasamp.Tests
     public class StringExtensionsTests
     {
         [TestMethod]
+        public void TestCountLowers()
+        {
+            // Only lowercase letters should be counted.
+            Assert.AreEqual(0, "ABACUS".CountLowers());
+            Assert.AreEqual(6, "abacus".CountLowers());
+            Assert.AreEqual(2, "abACUS".CountLowers());
+            Assert.AreEqual(3, "abaCU$".CountLowers());
+            Assert.AreEqual(2, "ab4CU$".CountLowers());
+        }
+
+        [TestMethod]
+        public void TestCountUppers()
+        {
+            // Only uppercase letters should be counted.
+            Assert.AreEqual(6, "ABACUS".CountUppers());
+            Assert.AreEqual(0, "abacus".CountUppers());
+            Assert.AreEqual(4, "abACUS".CountUppers());
+            Assert.AreEqual(2, "abaCU$".CountUppers());
+            Assert.AreEqual(2, "ab4CU$".CountUppers());
+        }
+
+        [TestMethod]
+        public void TestCountDigits()
+        {
+            // Only digits should be counted.
+            Assert.AreEqual(0, "ABACUS".CountDigits());
+            Assert.AreEqual(0, "abacus".CountDigits());
+            Assert.AreEqual(0, "abACUS".CountDigits());
+            Assert.AreEqual(0, "abaCU$".CountDigits());
+            Assert.AreEqual(1, "ab4CU$".CountDigits());
+        }
+
+        [TestMethod]
+        public void TestCountOthers()
+        {
+            // Only digits should be counted.
+            Assert.AreEqual(0, "ABACUS".CountOthers());
+            Assert.AreEqual(0, "abacus".CountOthers());
+            Assert.AreEqual(0, "abACUS".CountOthers());
+            Assert.AreEqual(1, "abaCU$".CountOthers());
+            Assert.AreEqual(1, "ab4CU$".CountOthers());
+        }
+
+        [TestMethod]
+        public void TestCountClasses()
+        {
+            // There are 0 classes in the empty string.
+            Assert.AreEqual(0, "".CountClasses());
+
+            // There is 1 class in each of these 1-character strings.
+            Assert.AreEqual(1, "1".CountClasses());
+            Assert.AreEqual(1, "a".CountClasses());
+            Assert.AreEqual(1, "A".CountClasses());
+            Assert.AreEqual(1, "$".CountClasses());
+
+            // This string contains all classes (a total of 4).
+            Assert.AreEqual(4, "1aA$".CountClasses());
+
+            // This string contains multiple instances of each class, but class count should still be 4.
+            Assert.AreEqual(4, "123abcABC$%^".CountClasses());
+        }
+
+        [TestMethod]
+        public void TestCountWords()
+        {
+            // There are 0 words in the empty string.
+            Assert.AreEqual(0, "".CountWords());
+
+            // There are 0 words in a string consisting only of non-letters.
+            Assert.AreEqual(0, "1234@#$123".CountWords());
+
+            // There is 1 word in a string consisting only of letters of the same case.
+            Assert.AreEqual(1, "abacus".CountWords());
+            Assert.AreEqual(1, "ABACUS".CountWords());
+
+            // There is 1 word in a string consisting only of mixed-case letters.
+            Assert.AreEqual(1, "aBAcus".CountWords());
+
+            // There is 1 word in a string with non-letters at the beginning, end or both.
+            Assert.AreEqual(1, "!abacus".CountWords());
+            Assert.AreEqual(1, "abacus!".CountWords());
+            Assert.AreEqual(1, "!abacus!".CountWords());
+
+            // There are multiple words in strings separated by non-letter characters.
+            Assert.AreEqual(2, "abacus!aardvark".CountWords());
+            Assert.AreEqual(2, "abacus!aardvark!".CountWords());
+            Assert.AreEqual(3, "abacus!aardvark8amulet".CountWords());
+            Assert.AreEqual(3, "!abacus!aardvark8amulet".CountWords());
+        }
+
+        [TestMethod]
         public void TestGetRepetitions()
         {
             // Repetitions in an empty string should be 0.
